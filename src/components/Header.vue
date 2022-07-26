5bda4ccdaf1b4e631ba2eb81e2f3e4aeeaf57f3f
Vegadrone <spenaantonino@gmail.com>
2022-07-26 14:32:22 +0100

    Craeate HeaderComponents navbar and jumbotron

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,20 @@
     <h1 class="text-uppercase fw-bold">
         header
     </h1>
+    <Navbar />
+    <Jumbotron />
   </header>
 </template>
 
 <script>
+import Navbar from "./HeaderComponents/Navbar.vue";
+import Jumbotron from "./HeaderComponents/Jumbotron.vue";
+
 export default {
+    components: {
+    Navbar,
+    Jumbotron
+  },
 
 }
 </script>
---
+++ src/components/HeaderComponents/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <div class="up-nav-bar">placeholder text</div>
+    <div class="nav-bar">
+        <ul>
+            <li>
+                menu voices
+            </li>
+        </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
