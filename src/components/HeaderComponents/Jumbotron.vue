2f48452933ef2d089a36d626388df7e76efe822b
Vegadrone <spenaantonino@gmail.com>
2022-07-26 16:43:33 +0100

    jumbotron basic layout complete, header basic layout complete

--- src/components/HeaderComponents/Jumbotron.vue
+++ src/components/HeaderComponents/Jumbotron.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="jumbo">
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-wrap">
+          <h1 class="text-bold text-light">Drive with Avada</h1>
+          <p class="text-light">
+            We offer the finest driving tuition money can buy
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+.jumbo {
+  background-image: url(../../assets/siteAssets/homepage-hero-background.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 
+.text-wrap{
+  padding: 15rem 0rem;
+}
 </style>
